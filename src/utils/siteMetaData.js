3027a6e9f3469d9993736f7314f092d3a24bba07
weimjay <weimjay@gmail.com>
2024-04-16 19:43:02 +1000

    first commit

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'CodeMaster',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'weimjay@gmail.com',
+    github: 'https://github.com/weimjay',
+    twitter: 'https://twitter.com',
+    facebook: 'https://facebook.com',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/',
+    dribbble: 'https://www.dribbble.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata
